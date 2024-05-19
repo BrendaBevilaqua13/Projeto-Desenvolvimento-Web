1729b5fd8ec02bd7f0f73d60713439451bbf5dd2
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2024-05-19 14:43:11 -0300

    criando as urls de list e detail das receitas

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -4,5 +4,6 @@ from recipes import views
 app_name = 'recipes'
 
 urlpatterns = [
-    path('recipes/api/v1/',views.RecipeListView.as_view(),name='recipes_api_v1')
+    path('recipes/api/v1/',views.RecipeListView.as_view(),name='recipes_api_v1'),
+    path('recipes/api/v1/<int:pk>/',views.RecipeDetailView.as_view(),name='recipes_api_v1_detail'),
 ]
