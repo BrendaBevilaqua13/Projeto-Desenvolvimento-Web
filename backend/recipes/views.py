e68e502b683f955e234129ccee4e8e5e8cbeec5f
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2024-05-21 17:30:11 -0300

    criando as views de autlizar e deletar receita

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -5,10 +5,12 @@ from django.shortcuts import get_list_or_404,get_object_or_404
 from django.template.defaultfilters import slugify
 from rest_framework.views import APIView 
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from .models import Recipe
 
 
 class RecipeListView(APIView):
+    permission_classes = (IsAuthenticated,)
     def get(self, request, *args, **kwargs):
         recipes = Recipe.objects.all()
         serializers = RecipeSerializer(recipes,many=True)
@@ -24,10 +26,26 @@ class RecipeListView(APIView):
         return Response(serializer.data,status=status.HTTP_201_CREATED)
 
 class RecipeDetailView(APIView):
+    permission_classes = (IsAuthenticated,)
     def get(self, request, *args, **kwargs):
         recipe = get_object_or_404(Recipe,pk=self.kwargs['pk'])
         serializers = RecipeSerializer(recipe)
         return Response(serializers.data)
     
+    def patch(self, request, *args, **kwargs):
+        recipe = get_object_or_404(Recipe,pk=self.kwargs['pk'])
+        serializer = RecipeSerializer(instance=recipe,
+                                       data=request.data,
+                                       partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(
+            author_id=1,category_id=1
+        )
+        return Response(serializer.data)
+    
+    def delete(self, request, *args, **kwargs):
+        recipe = get_object_or_404(Recipe,pk=self.kwargs['pk'])
+        recipe.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
         
