4674eab51a752071ff7b01ec6575118c9c0875d1
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2024-05-21 17:29:27 -0300

    criando as urls de autenticação

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from recipes import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 app_name = 'recipes'
 
 urlpatterns = [
     path('recipes/api/v1/',views.RecipeListView.as_view(),name='recipes_api_v1'),
     path('recipes/api/v1/<int:pk>/',views.RecipeDetailView.as_view(),name='recipes_api_v1_detail'),
+    path('recipes/api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('recipes/api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('recipes/api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
